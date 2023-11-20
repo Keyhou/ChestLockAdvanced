9cf1740ff28a270ec6ce9ae4a5fa770292eaf037
Keyhou <31210145+Keyhou@users.noreply.github.com>
2023-11-20 13:58:32 +0100

    Initial Commit

---
+++ ChestLockAdvanced/ChestLockAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChestLockAdvancedApp.swift
+//  ChestLockAdvanced
+//
+//  Created by Keyhan Mortezaeifar on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChestLockAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
